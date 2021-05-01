b87115d1487604450d3ac7e7257aba3c20d1480f
Minesh Varu <minesh.varu@gmail.com>
2021-05-01 15:08:51 -0400

    SEO enhancements
    - Adding google tag manager
    - Adding check to verify production vs development

--- components/Head/Head.js
+++ components/Head/Head.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { environment } from '../../environments';
 
 const SeoHead = ({province}) => {
     return (
@@ -13,6 +14,28 @@ const SeoHead = ({province}) => {
             <link rel="apple-touch-icon" href="/assets/favicon.ico" />
             <script async defer src="/assets/js/fontawesome.min.js"></script>
             <script async defer src="/assets/js/regular.min.js"></script>
+
+            {
+                process.env.NEXT_PUBLIC_ENV === 'production' ?
+                <script  async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${environment.tagId}`}>
+                </script>
+                
+                : null
+            }
+
+{
+            process.env.NEXT_PUBLIC_ENV === 'production' ?
+            <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer', '${environment.tagId}' );`}}>
+
+                </script>
+                
+                : null
+            }
         </Head>
     )
 }
--- environments.js
+++ environments.js
@@ -1,11 +1,15 @@
 const environments = {
     local : {
+        env: 'local',
         cdnServerUrl: 'http://localhost:3000',
-        baseServerUrl: 'http://localhost:8080/api/v1'
+        baseServerUrl: 'http://localhost:8080/api/v1',
+        tagId: ''
     },
     production: {
+        env: 'prod',
         cdnServerUrl: 'https://canadacovidtracker.com',
         baseServerUrl: 'https://api.canadacovidtracker.com/api/v1',
+        tagId: 'G-94QR6QF10C'
     }
 }
 
